b0b107f6e3a1efe5287676590a82e8c3b73b856c
haseena-pa <haseenakhader.uk@gmail.com>
2022-06-19 13:59:42 +0100

    form-validation in vanilla js

---
+++ form-validation/script.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmPassword');
+
+
+function showError(fieldName) {
+    fieldName.parentElement.className = 'form-control error';
+    fieldName.nextElementSibling.innerHTML = `${capitaliseFieldName(fieldName)} is required`;
+}
+
+function showSuccess(fieldName) {
+    fieldName.parentElement.className = 'form-control success';
+}
+
+function capitaliseFieldName(fieldName) {
+    return fieldName.id.charAt(0).toUpperCase() + fieldName.id.slice(1);
+}
+
+function checkFieldValue(fieldArr) {
+    fieldArr.forEach(element => {
+        if (element.value === '') {
+            showError(element);
+        } else {
+            showSuccess(element);
+        }
+    });
+}
+
+document.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkFieldValue([username, email, password, confirmPassword]);
+})
